ba57045ad2054eb733731f6c0a00d09ae6564389
Akonwi Ngoh <akonwi@gmail.com>
2026-05-30 17:01:59 -0400

    ard v0.21.1

--- Formula/ard.rb
+++ Formula/ard.rb
@@ -1,24 +1,24 @@
 class Ard < Formula
   desc "Programming language and compiler"
   homepage "https://github.com/akonwi/ard"
-  version "0.21.0"
+  version "0.21.1"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/akonwi/ard/releases/download/v0.21.0/ard_v0.21.0_darwin_arm64.tar.gz"
-      sha256 "7e483c8fa61121024e9fa0c35167dcac67a2aa62a7ea0802318d8eb855204701"
+      url "https://github.com/akonwi/ard/releases/download/v0.21.1/ard_v0.21.1_darwin_arm64.tar.gz"
+      sha256 "5e5fd557def2f352bfffdbb1a0fd9f3eef5c7740c9bb47f51509b2e21557d8f5"
     else
-      url "https://github.com/akonwi/ard/releases/download/v0.21.0/ard_v0.21.0_darwin_amd64.tar.gz"
-      sha256 "521dc9548678882717cb66c843bd9b758cb0d16085492ff4dd06255a4595937d"
+      url "https://github.com/akonwi/ard/releases/download/v0.21.1/ard_v0.21.1_darwin_amd64.tar.gz"
+      sha256 "1ecdf4056e0c50d4b8fae4b568bfc2af6bcc0876359129dbca49fdece4b16993"
     end
   elsif OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/akonwi/ard/releases/download/v0.21.0/ard_v0.21.0_linux_arm64.tar.gz"
-      sha256 "b5da95ee7d81bd66bb776cea79ef0adea109df69eb3710a5c1c417f40a193f85"
+      url "https://github.com/akonwi/ard/releases/download/v0.21.1/ard_v0.21.1_linux_arm64.tar.gz"
+      sha256 "20609a7ea4460a8274f76423ea7cdba65f9ac4730d1e40fd62f7c1740d102209"
     else
-      url "https://github.com/akonwi/ard/releases/download/v0.21.0/ard_v0.21.0_linux_amd64.tar.gz"
-      sha256 "2853e3b57325e52601bbb223422d2f4406e8301c5547c98534f9e94fa30ad71a"
+      url "https://github.com/akonwi/ard/releases/download/v0.21.1/ard_v0.21.1_linux_amd64.tar.gz"
+      sha256 "d46d7152d2a6527ec31065e4ec6cd2fc61686871eecb6a44cc06ee61d967fbcc"
     end
   end
 
@@ -27,6 +27,6 @@ class Ard < Formula
   end
 
   test do
-    assert_match "v0.21.0", shell_output("#{bin}/ard version")
+    assert_match "v0.21.1", shell_output("#{bin}/ard version")
   end
 end
