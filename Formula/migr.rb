057deae8ad9291d66594c095394df0128cc89f8a
Akonwi Ngoh <akonwi@gmail.com>
2026-07-13 21:39:16 -0400

    migr v0.1.1

--- Formula/migr.rb
+++ Formula/migr.rb
@@ -1,24 +1,24 @@
 class Migr < Formula
   desc "Database migration tool"
   homepage "https://github.com/akonwi/migr"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/akonwi/migr/releases/download/v0.1.0/migr-darwin-arm64.tar.gz"
-      sha256 "7239a3191a2d1476c02099aa44b820ea3dac143661fd670d174a4a3dc7d0c6bd"
+      url "https://github.com/akonwi/migr/releases/download/v0.1.1/migr-darwin-arm64.tar.gz"
+      sha256 "3c6f9c86829130ace191946f384fe85ff61cc6ae132696d0e483a595d9673db3"
     else
-      url "https://github.com/akonwi/migr/releases/download/v0.1.0/migr-darwin-amd64.tar.gz"
-      sha256 "48643b95a1c1d487bd190b09431cf834c149b47c1587f37d6fe8adcef6ee5718"
+      url "https://github.com/akonwi/migr/releases/download/v0.1.1/migr-darwin-amd64.tar.gz"
+      sha256 "47ff875abc57ffa0c6a83980fc714e410fc3fe108bf31996d4c3929323fc2a5d"
     end
   elsif OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/akonwi/migr/releases/download/v0.1.0/migr-linux-arm64.tar.gz"
-      sha256 "9ddcc377b55ff5a08a0462a91416b1425d9a887880ec78fc26322aa7cb9b0036"
+      url "https://github.com/akonwi/migr/releases/download/v0.1.1/migr-linux-arm64.tar.gz"
+      sha256 "49328783ed5fa126554e422cdd04cb90081ba3111baedea39c960af07fae7da3"
     else
-      url "https://github.com/akonwi/migr/releases/download/v0.1.0/migr-linux-amd64.tar.gz"
-      sha256 "69b1dd03e4abb1435fd39c4da431749716283579ef9e3226f714b5482a18e015"
+      url "https://github.com/akonwi/migr/releases/download/v0.1.1/migr-linux-amd64.tar.gz"
+      sha256 "05d693b8de1ede54db6c80032b07a691994003cfc5fef6c4cd453bf73f220e17"
     end
   end
 
