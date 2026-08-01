5bc2bdfc5fa107864f764e5770b554f300fbd22d
Akonwi Ngoh <akonwi@gmail.com>
2026-08-01 13:57:55 +0200

    kit 0.25.0

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -1,24 +1,24 @@
 class Kit < Formula
   desc "Terminal-first coding agent"
   homepage "https://github.com/akonwi/kit"
-  version "0.24.0"
+  version "0.25.0"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/akonwi/kit/releases/download/v0.24.0/kit_v0.24.0_darwin_arm64.tar.gz"
-      sha256 "c2c446f0cfadc3d620b26cbd2113e9f050ed75ff880fd1796f57bad51755aed3"
+      url "https://github.com/akonwi/kit/releases/download/v0.25.0/kit_v0.25.0_darwin_arm64.tar.gz"
+      sha256 "8b6c7aa0caecf1cc078523b3326e241a7dd3ea1bc16d9988de5fe725dd6af872"
     else
-      url "https://github.com/akonwi/kit/releases/download/v0.24.0/kit_v0.24.0_darwin_amd64.tar.gz"
-      sha256 "e40236e3621df59abfd4898c3e8858f533314efa459aeb3cac9094e6747158d6"
+      url "https://github.com/akonwi/kit/releases/download/v0.25.0/kit_v0.25.0_darwin_amd64.tar.gz"
+      sha256 "43dcd96b607d18a3343b57b3dde6f4c0ccace0fea3ed90f3d39ee24d4d1f2b93"
     end
   elsif OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/akonwi/kit/releases/download/v0.24.0/kit_v0.24.0_linux_arm64.tar.gz"
-      sha256 "6c815dcb1248ca2f8390d8840d670333409cecd55702ddbd3ffd29a63e3cb2eb"
+      url "https://github.com/akonwi/kit/releases/download/v0.25.0/kit_v0.25.0_linux_arm64.tar.gz"
+      sha256 "6bdcf6eea73377538f630b8a49495fcbaccd480f41b2a00966c247916bf95f05"
     else
-      url "https://github.com/akonwi/kit/releases/download/v0.24.0/kit_v0.24.0_linux_amd64.tar.gz"
-      sha256 "94078b60154b52cc198e85ef3e935872e86dccb24814d34ef2df206a8a5a09b7"
+      url "https://github.com/akonwi/kit/releases/download/v0.25.0/kit_v0.25.0_linux_amd64.tar.gz"
+      sha256 "c749756ce59c2797a7546899dc4b0f9d0dc7a6941cc4f43b30dd6a0f624421b6"
     end
   end
 
@@ -31,6 +31,6 @@ class Kit < Formula
   end
 
   test do
-    assert_match "v0.24.0", shell_output("#{bin}/kit version")
+    assert_match "v0.25.0", shell_output("#{bin}/kit version")
   end
 end
